8df3543be4bd90656a9448a4a608396f1a0eb0f4
W3Bches <88248105+W3Bches@users.noreply.github.com>
2021-08-27 23:08:26 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function discord(){
 	location.href='https://discord.gg/w4jHYMuHj9'
 }
-function discord(){
+function telegram(){
 	location.href='https://t.me/w3bches'
+}
+function youtube(){
+	location.href='https://youtube.com/channel/UCpQiUs7EVj91sIku9kvlw4w'
 }
